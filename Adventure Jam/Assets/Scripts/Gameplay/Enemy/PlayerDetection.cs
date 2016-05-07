6fff74871e1db1641d73e1bd5fd15c20882a0378
Matías Montenegro <matias@framematt.com>
2016-05-07 20:39:11 -0300

    Enemy IA
    - Enemigo detecta y va a atacar al jugador.
    - Problemas en un rayo de deteccion EnemyAI.cs (pa variar)

---
+++ Adventure Jam/Assets/Scripts/Gameplay/Enemy/PlayerDetection.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerDetection : MonoBehaviour
+{
+	/* Atributos */
+	[Header ("Player detector")]
+	public bool playerDetected;
+
+	/* Triggers */
+	void OnTriggerEnter (Collider other) {
+		if (other.tag == "Player") {
+			playerDetected = true;
+		}
+	}
+
+	void OnTriggerExit (Collider other) {
+		if (other.tag == "Player") {
+			playerDetected = false;
+		}
+	}
+}
---
+++ Adventure Jam/Assets/Scripts/Gameplay/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+	/* Atributos */
+	public static PlayerController instance;
+	public Transform selfTrans;
+
+	/* Aplicacion al motor */
+	void Awake () {
+		instance = this;
+	}
+
+	void Start () {
+		selfTrans = this.transform;
+	}
+}
