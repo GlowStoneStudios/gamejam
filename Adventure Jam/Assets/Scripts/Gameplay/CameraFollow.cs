fa3e70c5c269cc3944c5adef53c8fb8f3d559020
deadlykrow <f.mendezmen1@gmail.com>
2016-05-08 02:46:04 -0300

    avances sabado 07-05
    inicio de player controller, sus fundamentos y ademas inicio de camera
    follow para el player y esceneas de corte o posicionamiento manual de
    camara

---
+++ Adventure Jam/Assets/Scripts/Gameplay/CameraFollow.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+    Transform cached;
+    public bool FollowPlayer = true;
+    public Vector3 TargetPositionOffset;
+    public Vector3 TargetPosition { get ; private set;} 
+    public Vector3 LookAtPosition { get ; private set;}
+    public float FollowSpeed = 3f, RotationSmooth = 3f;
+
+	// Use this for initialization
+	void Awake () {
+        cached = this.transform;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        if (FollowPlayer)
+        {
+            Quaternion rotation = Quaternion.LookRotation(PlayerController.instance.cached.position - cached.position);
+
+            cached.rotation = Quaternion.Slerp(cached.rotation, rotation, Time.deltaTime * RotationSmooth);
+             
+            cached.position = Vector3.Lerp(cached.position, (PlayerController.instance.cached.position + TargetPositionOffset), Time.deltaTime * FollowSpeed);
+        }
+        else
+        {
+            Quaternion rotation = Quaternion.LookRotation(TargetPosition - cached.position);
+            cached.rotation = Quaternion.Slerp(cached.rotation, rotation, Time.deltaTime * RotationSmooth);
+
+            cached.position = Vector3.Lerp(cached.position, (TargetPosition - TargetPositionOffset), Time.deltaTime * FollowSpeed);
+        }
+	}
+}
--- Adventure Jam/Assets/Scripts/Gameplay/PlayerController.cs
+++ Adventure Jam/Assets/Scripts/Gameplay/PlayerController.cs
@@ -3,16 +3,101 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
-	/* Atributos */
+    
 	public static PlayerController instance;
-	public Transform selfTrans;
+    public  Transform cached { get; private set; }
+   
+    public enum playerCurState
+    {
+        Idle, Walk, SideWalk, BackWalk, Run, Jump, Crouch, Action, Grab
+    }
 
-	/* Aplicacion al motor */
-	void Awake () {
+    [Header("Movimiento")]
+
+    public bool canMove = true;
+    public playerCurState playerState;
+    public float WalkSpeed = 3f, RunSpeed = 7f;
+    float curSpeed = 0f;
+
+    float aX, aY;
+    bool jumpKey, runKey;
+
+    [Header("Rol")]
+    public float Health = 100f;
+    public float Mana = 100f;
+
+
+
+
+	void Awake (){
 		instance = this;
+        cached = this.transform;
 	}
 
-	void Start () {
-		selfTrans = this.transform;
-	}
+    void Update(){
+
+        aX = Input.GetAxisRaw("Horizontal");
+        aY = Input.GetAxisRaw("Vertical");
+
+        MovementCore();
+    }
+    void MovementCore()
+    {
+        if (canMove)
+        {
+
+            jumpKey = Input.GetKey(KeyCode.Space);
+            runKey = Input.GetKey(KeyCode.LeftShift);
+
+            //Logica de movimiento forward y backward
+
+            if(aY != 0)
+            {
+                if (aY > 0)
+                {
+                    if (runKey)
+                    {
+                        playerState = playerCurState.Run;
+                        curSpeed = RunSpeed;
+                    }
+                    else
+                    {
+                        playerState = playerCurState.Walk;
+                        curSpeed = WalkSpeed;
+                    }
+                }
+                else
+                {
+                    playerState = playerCurState.BackWalk;
+                    curSpeed = WalkSpeed;
+                }
+
+                cached.Translate(Vector3.forward * aY *curSpeed * Time.deltaTime);
+
+            }
+            // Logica de movimiento lateral
+
+            if(aX != 0)
+            {
+                if (aY != 0)
+                {
+                    cached.Translate(Vector3.right * aX * curSpeed * 0.666f * Time.deltaTime);
+                }
+                else
+                {
+                    playerState = playerCurState.SideWalk;
+                    curSpeed = WalkSpeed;
+                    cached.Translate(Vector3.right * aX * curSpeed * 0.666f * Time.deltaTime);
+                }
+            }
+
+            //Volver a idle cuando los ejes son 0
+
+            if (aX == 0 && aY == 0)
+            {
+                curSpeed = 0f;
+                playerState = playerCurState.Idle;
+            }
+        }
+    }
 }
