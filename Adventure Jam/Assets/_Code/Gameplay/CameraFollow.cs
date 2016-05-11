2b26441ef1db292e383b265e13351421f3fcc5af
deadlykrow <f.mendezmen1@gmail.com>
2016-05-11 01:17:57 -0300

    Avances martes, desde la casita :B
    terreno, piedras

---
+++ Adventure Jam/Assets/_Code/Gameplay/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+    Transform cached;
+
+    public Transform TargetPosition;
+    public Transform LookAtTarget;
+    public float FollowSpeed = 3f, RotationSmooth = 3f;
+
+	void Awake () {
+        cached = this.transform;
+	}
+
+	void LateUpdate () {
+
+        Quaternion rotation = Quaternion.LookRotation(LookAtTarget.position - cached.position);
+            cached.rotation = Quaternion.Slerp(cached.rotation, rotation, Time.deltaTime * RotationSmooth);
+        cached.position = Vector3.Lerp(cached.position, (TargetPosition.position), Time.deltaTime * FollowSpeed);
+
+	}
+}
